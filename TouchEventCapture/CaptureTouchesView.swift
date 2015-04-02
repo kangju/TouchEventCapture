758b3494d09eb342796728ab5b0e88371e0f0d02
FujiwaraYasunori <kangju@FujiwaraYasunori-no-MacBook-Air.local>
2015-04-02 20:13:21 +0900

    create CaptureTouchesView class file

---
+++ TouchEventCapture/CaptureTouchesView.swift
@@ -0,0 +1,9 @@
+//
+//  CaptureTouchesView.swift
+//  TouchEventCapture
+//
+//  Created by FujiwaraYasunori on 2015/04/02.
+//  Copyright (c) 2015年 FujiwaraYasunori. All rights reserved.
+//
+
+import Foundation
